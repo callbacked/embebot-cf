cf8de0a69ff9ff5081fde195737fe7481dceeb6f
callbacked <alexanderjunior2478@gmail.com>
2025-12-28 01:33:36 -0500

    add custom endpoint support per server settings

---
+++ drizzle/0001_melted_vector.sql
@@ -0,0 +1,5 @@
+ALTER TABLE "server_settings" ADD COLUMN "twitter_endpoint" text;--> statement-breakpoint
+ALTER TABLE "server_settings" ADD COLUMN "x_endpoint" text;--> statement-breakpoint
+ALTER TABLE "server_settings" ADD COLUMN "tiktok_endpoint" text;--> statement-breakpoint
+ALTER TABLE "server_settings" ADD COLUMN "instagram_endpoint" text;--> statement-breakpoint
+ALTER TABLE "server_settings" ADD COLUMN "reddit_endpoint" text;
